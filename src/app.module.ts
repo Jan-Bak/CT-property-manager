c5ce4c85fb8794aeab75a244a7b3727ca1caf6d4
BerryJohn <jan.pawel.bak01@gmail.com>
2026-03-31 00:42:02 +0200

    added a nestjs/config to load .env data for proper config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,27 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PropertiesModule } from './modules/properties/properties.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
       playground: true,
     }),
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/property-manager',
-    ),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGODB_URI') ||
+          'mongodb://localhost:27017/property-manager',
+      }),
+    }),
     PropertiesModule,
   ],
 })
