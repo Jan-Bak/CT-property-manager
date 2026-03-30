cdcea845132a7b94689211b47eaa997a92a7b928
BerryJohn <jan.pawel.bak01@gmail.com>
2026-03-30 22:59:08 +0200

    Installed dependencies to graphql/mongo, mongo on docker, server is running (added a resolve) with grapql playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+      playground: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/property-manager',
+    ),
+  ],
+  providers: [AppResolver],
 })
 export class AppModule {}
